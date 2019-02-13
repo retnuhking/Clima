189f363d1dff1656f8954fe676f3b3a4d6b2906b
retnuhking <44440921+retnuhking@users.noreply.github.com>
2019-02-13 14:31:17 -0800

    Add all unversioned project files

---
+++ app/src/main/java/com/londonappbrewery/climapm/SettingsController.java
@@ -0,0 +1,47 @@
+package com.londonappbrewery.climapm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import static com.londonappbrewery.climapm.WeatherController.getCurrentCity;
+
+public class SettingsController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_layout);
+        ImageButton backButton = findViewById(R.id.settingsBackButton);
+        Switch windToggle = findViewById(R.id.windTS);
+        final TextView mWindSpeedTV = findViewById(R.id.windSpeedTV);
+
+        final Intent toggleIntent = new Intent(SettingsController.this, WeatherController.class);
+
+        windToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                if (isChecked) {
+                    toggleIntent.putExtra("WindVis", true);
+                } else {
+                    toggleIntent.putExtra("WindVis", false);
+                }
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String currentCity = getCurrentCity();
+                toggleIntent.putExtra("City", currentCity);
+                startActivity(toggleIntent);
+            }
+        });
+    }
+}
